912519cfc49df9e7062da04aebe49717fd2c5c56
amalgamaria <madonay@clemson.edu>
2021-12-06 15:27:25 -0500

    Add command to remove tmp .gitkeep file

--- DAM_blips_package/scripts/blips_initiator.sh
+++ DAM_blips_package/scripts/blips_initiator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p ../output/{error,output_csv,preview} | sbatch ./blips_shscript.sh
+rm ../data/.gitkeep; mkdir -p ../output/{error,output_csv,preview} | sbatch ./blips_shscript.sh
