7ed57741121cdf3b436d706c2ec5ef82e4aeb5be
Janet Pan <ypan23@uw.edu>
2022-01-27 12:27:56 -0500

    edit file name in initiators

--- longitudinal/scripts/longitudinal_initiator.sh
+++ longitudinal/scripts/longitudinal_initiator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p ../output/{error,output_csv,preview} | sbatch ./blips_shscript.sh
+mkdir -p ../output/{error,output_csv,preview} | sbatch ./longitudinal_shscript.sh
--- streamline2.0/scripts/streamline_initiator2.0.sh
+++ streamline2.0/scripts/streamline_initiator2.0.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p ../output/{error,output_csv,preview} | sbatch ./dam_shscript.sh
+mkdir -p ../output/{error,output_csv,preview} | sbatch ./streamline_shscript2.0.sh
